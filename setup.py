69d3fc52dcf003f548b6f0052b07015aa43e89d6
nhamngoctuananh <vucute9366@yahoo.com.vn>
2022-02-15 16:35:58 +0700

    now

--- setup.py
+++ setup.py
@@ -7,18 +7,17 @@ setup(ext_modules=cythonize(ext, language_level = "3"))
 
 
 
-#ext = Extension(name="my_module", sources=["my_module.pyx"])
-# setup(
-#     ext_modules=[
-#         Extension("module", ["module.c"],
-#                   include_dirs=[numpy.get_include()]),
-#     ],
-# )
+#setup(
+    #ext_modules=[
+        #Extension("module", ["module.c"],
+                  #include_dirs=[numpy.get_include()]),
+    #],
+#)
+
 # Or, if you use cythonize() to make the ext_modules list,
 # include_dirs can be passed to setup()
 
-# setup(
-#     ext_modules=cythonize("module.pyx"),
-#     include_dirs=[numpy.get_include()]
-# )
-#setup(ext_modules=cythonize(ext, language_level = "3"))
+#setup(
+    #ext_modules=cythonize("module.pyx"),
+    #include_dirs=[numpy.get_include()]
+#)
